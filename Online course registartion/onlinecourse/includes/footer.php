2c4d515b49060a3fbb6c863d4eb4ced980e0539f
Naveen R <53350757+naveen672@users.noreply.github.com>
2022-03-16 09:27:20 +0530

    Add files via upload

---
+++ Online course registartion/onlinecourse/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2015 Online Course Registration | By : NAVEEN</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
